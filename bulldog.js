80c77543ed14220e0e95e22ef1627706b24c207f
Pablo Novas <pjnovas@gmail.com>
2012-07-14 18:00:12 -0300

    added ON events

--- bulldog.js
+++ bulldog.js
@@ -1,8 +1,47 @@
-var Bulldog = module.exports = function Bulldog(url, interval){
+
+var request = require('request');
+
+
+function Bulldog(url, interval){
 	this.url = url;
 	this.interval = interval;
+	this.events = {};
+	this.lastBody = "";
+
+	function makeRequest(){
+
+		if (this.events["look"]){
+			this.events["look"]();
+		}
+
+		request(url, function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+		  	if (body != lastBody){
+			    if (this.events["change"]){
+						this.events["change"](body);
+					}
+				}
+		  }
+		  else {
+		  	if (this.events["error"]){
+					this.events["error"](error, response.statusCode);
+				}
+		  }
+		});
+	}
+
+	this.timer = setInterval(makeRequest, interval);
 };
+
+module.exports = Bulldog;
 
 Bulldog.watch = function(url, interval){
 	return new Bulldog(url, interval);
-}
+};
+
+Bulldog.prototype = {
+	on: function(name, cb){
+		this.events[name] = cb;
+		return this;
+	}
+};
