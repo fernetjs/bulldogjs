c1706104085ff7028010ae49087e92dd2bb5a22b
Pablo Novas <pjnovas@gmail.com>
2012-07-14 22:01:12 -0300

    refactor makeRequest

--- bulldog.js
+++ bulldog.js
@@ -10,62 +10,21 @@ function Bulldog(url, interval, opt){
 	this.lastBody = "";
 
 	var self = this;
-	function makeRequest(){
-
-		request(url, function (error, response, body) {
-			if (self.events["look"]){
-				self.events["look"](body);
-			}
-
-		  if (!error && response.statusCode == 200) {
-		  	if (body != self.lastBody && self.events['change']) {
-
-		  		var changeEvents = self.events['change'];
-		  		for(var i=0; i<changeEvents.length; i++) {
-		  			if (!changeEvents[i].selector) {
-		  				changeEvents[i].callback(body, self.lastBody);
-		  				break;
-		  			}
-		  		}
-		  		self.lastBody = body;
-
-		  		if (self.events['change'] && self.events['change'].length > 0) {
-
-		  			var jsdom    = require("jsdom").jsdom,
-						    document = jsdom(body, null, { features: { QuerySelector : true	}}), 
-						    window   = document.createWindow();
-
-		  			for(var i=0; i<changeEvents.length; i++) {
-		  				
-		  				var result = window.document.querySelectorAll(changeEvents[i].selector);
-		  				var lastResult = changeEvents[i].lastResult;
-		  				
-		  				if (result != lastResult){
-		  					changeEvents[i].callback(result, lastResult);
-		  					if (changeEvents[i])
-		  						changeEvents[i].lastResult = result;
-		  				}
-		  			}
-
-		  			window.close();
-					}
-				}
-		  }
-		  else {
-		  	if (self.events["error"]){
-					self.events["error"](error, response.statusCode);
-				}
-		  }
-		});
-	}
-
-	this.timer = setInterval(makeRequest, interval);
+	this.timer = setInterval(function(){
+		self.makeRequest();
+	}, interval);
 };
 
 module.exports = Bulldog;
 
 Bulldog.watch = function(url, interval, opt){
 	return new Bulldog(url, interval, opt);
+};
+
+Bulldog.stopWatch = function(){
+	clearInterval(this.timer);
+	//TODO: destroy or has a startWatch to resume?
+	return this;
 };
 
 Bulldog.prototype = {
@@ -94,11 +53,12 @@ Bulldog.prototype = {
 	off: function(name, selector){
 		if (this.events[name]) {
 			
-			if (name === 'change' && typeof selector == "string") {
+			if (name === 'change') {
 				var changeEvents = this.events[name];
+
 				for (var i=0; i< changeEvents.length; i++){
-					if (changeEvents[i].selector === selector){
-						this.events[name].splice(i, 1);
+					if (changeEvents[i].selector === selector) {
+							this.events[name].splice(i, 1);
 						break;
 					}
 				}
@@ -108,5 +68,56 @@ Bulldog.prototype = {
 		}	
 
 		return this;
+	},
+	makeRequest: function(){
+		var self = this;
+
+		request(self.url, function (error, response, body) {
+			if (self.events["look"]){
+				self.events["look"](body);
+			}
+
+		  if (!error && response.statusCode == 200) {
+	  		self.updateChangeEvents(body);
+		  }
+		  else {
+		  	if (self.events["error"]){
+					self.events["error"](error, response.statusCode);
+				}
+		  }
+		});
+	},
+	updateChangeEvents: function(html){
+		var changeEvents = this.events['change'];
+		if (changeEvents && changeEvents.length > 0 && html != this.lastBody) {
+	
+			var window = this.initBrowserWindow(html);
+
+			for(var i=0; i<changeEvents.length; i++) {
+				
+				if (!changeEvents[i].selector) {
+					changeEvents[i].callback(html, this.lastBody);
+					this.lastBody = html;
+				}
+				else {
+					var result = window.document.querySelectorAll(changeEvents[i].selector);
+					var lastResult = changeEvents[i].lastResult;
+					
+					if (result != lastResult){
+						changeEvents[i].callback(result, lastResult);
+						if (changeEvents[i])
+							changeEvents[i].lastResult = result;
+					}
+				}
+			}
+
+			window.close();
+		}
+	},
+	initBrowserWindow: function(html){
+    var document = jsdom.jsdom(html, null, { features: { QuerySelector : true	}}); 
+    return document.createWindow();
 	}
 };
+
+
